8d4d2c670956d9bced9824dec2e0f84daacf0229
Babatunde Mattieu <bbmattieu9@gmail.com>
2019-08-13 22:23:55 +0100

    Added components

--- ang-for-beginner/src/app/app.component.ts
+++ ang-for-beginner/src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+      <h1>{{pageTitle}}</h1>
+      <app-products></app-products>
+  </div>
+  `
 })
 export class AppComponent {
   title = 'Angular8: Getting Started';
