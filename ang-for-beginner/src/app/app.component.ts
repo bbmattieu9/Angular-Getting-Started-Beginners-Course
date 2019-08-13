03133cf90a2ef266cf316cf6fec98c6bb0f7b989
Babatunde Mattieu <bbmattieu9@gmail.com>
2019-08-13 18:00:25 +0100

    welcome component and shared component created

--- ang-for-beginner/src/app/app.component.ts
+++ ang-for-beginner/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-for-beginner';
+  title = 'Angular8: Getting Started';
 }
